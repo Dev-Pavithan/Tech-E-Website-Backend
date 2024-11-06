76531d6b4efdf8f3230afe6751624753eb83c879
Dev-Pavithan <pavithanunenthiran29@gmail.com>
2024-11-06 11:40:41 +0530

    tes6

--- server.js
+++ server.js
@@ -1,4 +1,61 @@
-// server.js
+// // server.js
+// import express from 'express';
+// import morgan from 'morgan';
+// import cors from 'cors';
+// import cookieParser from 'cookie-parser';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import helmet from 'helmet';
+// import errorHandler from './middleware/errorHandler.js';
+// import paymentRoutes from './routes/paymentRoutes.js'; 
+// import authRoutes from './routes/authRoutes.js';
+// import contactRoutes from './routes/contactRoutes.js';
+// import loginRoutes from './routes/loginRoutes.js';
+// import packageRoutes from './routes/packageRoutes.js';
+// import imageRoutes from './routes/imageRoutes.js';  
+
+
+// dotenv.config();
+
+// const app = express();
+
+// // Middleware
+// app.use(express.json());
+// app.use(morgan('dev'));
+// const corsOptions = {
+//   origin: 'https://dev-pavithan-tech-e-website-frontend.vercel.app',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   credentials: true
+// };
+
+// app.use(cors(corsOptions));
+// app.use(cookieParser());
+// app.use(helmet());
+
+// // Connect to MongoDB
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverSelectionTimeoutMS: 30000,
+// })
+// .then(() => console.log('MongoDB Connected'))
+// .catch(err => console.error('MongoDB connection error:', err));
+
+// // Use routes
+// app.use('/user', authRoutes);
+// app.use('/login', loginRoutes);
+// app.use('/contact', contactRoutes);
+// app.use('/api/packages', packageRoutes);
+// app.use('/api/payments', paymentRoutes);
+// app.use('/api/images', imageRoutes); 
+// // app.use('/api/admin-settings', adminSettingsRoutes);
+
+
+// // Custom error handling middleware
+// app.use(errorHandler);
+
+// const PORT = process.env.PORT || 7100;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
@@ -13,7 +70,6 @@ import contactRoutes from './routes/contactRoutes.js';
 import loginRoutes from './routes/loginRoutes.js';
 import packageRoutes from './routes/packageRoutes.js';
 import imageRoutes from './routes/imageRoutes.js';  
-
 
 dotenv.config();
 
@@ -48,11 +104,8 @@ app.use('/contact', contactRoutes);
 app.use('/api/packages', packageRoutes);
 app.use('/api/payments', paymentRoutes);
 app.use('/api/images', imageRoutes); 
-// app.use('/api/admin-settings', adminSettingsRoutes);
-
 
 // Custom error handling middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 7100;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
